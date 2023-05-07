5b0d649a22be29d4f48074f504b0a7c99fa50a57
Carlos Andres Firigua Contreras <71801587+clfirigua@users.noreply.github.com>
2023-05-07 10:08:36 -0500

    cambios

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-const imgUno = document.getElementById('imgUno');
-const imgDos = document.getElementById('imgDos');
-const imgTres = document.getElementById('imgTres');
-const imgTotal = document.getElementById('imgTotal');
+import { cards } from './assets/cards.js';
+
+const container = document.getElementById('conatainer');
+
+cards.forEach(card => {
+    $(container).append(`
+        <div class="card " style="width: 18rem; margin-left: 20px;">
+            <img src="${card.img}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${card.title}</h5>
+                <p class="card-text">${card.description}</p>
+                <a href="${card.url}" class="btn btn-primary">ver</a>
+            </div>
+        </div>
+    `);
+});
 
 
 
-imgUno.addEventListener('click',()=>{
-    imgTotal.src = imgUno.dataset.img;
 
-})
-imgDos.addEventListener('click',()=>{
-    imgTotal.src = imgDos.dataset.img;
 
-})
-imgTres.addEventListener('click',()=>{
-    imgTotal.src = imgTres.dataset.img;
-    
-})
+
